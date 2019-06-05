6ff8439b2d8f4fd68fc5f0e70b0db2635d0ad068
amusing-history-4188 <mackenzieerinpech@gmail.com>
2019-06-05 13:31:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-Function theBeatlesPlay(musician, instrument){
+Function theBeatlesPlay(musician, instrument) {
   var x = [];
   for(var i=0; i<musician.length; i++) {
     x.push (musician.[i] + " plays " + instrument.[i])
