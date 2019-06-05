2427688fffc71a6d51dcb1d9e27b1a03bcbfb2f1
amusing-history-4188 <mackenzieerinpech@gmail.com>
2019-06-05 13:45:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ function iLoveTheBeatles(number) {
     arr.push('I love the Beatles!');
     i++;
   } 
-  while (i <= number && number < 15);
+  while (i < number && number < 15);
   return arr;
 }
