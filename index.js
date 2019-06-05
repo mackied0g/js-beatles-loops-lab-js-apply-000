9cdac496b3737fc566bd4195e536ce0e2b8bf637
amusing-history-4188 <mackenzieerinpech@gmail.com>
2019-06-05 13:30:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 Function theBeatlesPlay(musician, instrument){
   var x = [];
   for(var i=0; i<musician.length; i++) {
-    x.push (musician.[i] + " plays " +
+    x.push (musician.[i] + " plays " + instrument.[i];
   }
 }
